cf84dd0bc6a62855a578d5bdeda9f49f14a49c55
Tyler <Tyler.lazarus01@outlook.com>
2023-07-10 03:22:10 +0100

    Commit 1 - Created Window with changable pixels, created circle and ability to move circle with inputs.

---
+++ axe_game/_build/axe_game.cpp
@@ -0,0 +1,29 @@
+#include "raylib.h"
+
+int main()
+{
+	int screenWidth = 350;
+	int screenHeight = 250;
+
+	int circleRadius = 25;
+	int circle_x = screenWidth / 2;
+	int circle_y = screenHeight / 2;
+
+	InitWindow(screenWidth, screenHeight, "Axe Game");
+
+	SetTargetFPS(60);
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(WHITE);
+
+		DrawCircle(circle_x, circle_y, circleRadius, RED);
+
+		if (IsKeyDown(KEY_W)) circle_y -= 1;
+		if (IsKeyDown(KEY_S)) circle_y += 1;
+		if (IsKeyDown(KEY_A)) circle_x -= 1;
+		if (IsKeyDown(KEY_D)) circle_x += 1;
+
+		EndDrawing();
+	}
+}
