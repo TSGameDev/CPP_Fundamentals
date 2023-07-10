b86b3e67260c1cd019f11359f0c60eb866a60cf3
Tyler <Tyler.lazarus01@outlook.com>
2023-07-10 13:36:32 +0100

    commit 4 - Finished axe game with collision detection using calculated bounds of elements.

--- axe_game/_build/axe_game.cpp
+++ axe_game/_build/axe_game.cpp
@@ -17,6 +17,8 @@ int main()
 	int movementSpeed = 5;
 	int squareMoveDir = 5;
 
+	bool endGameCollision = false;
+
 	InitWindow(screenWidth, screenHeight, "Axe Game");
 
 	SetTargetFPS(60);
@@ -24,17 +26,50 @@ int main()
 	{
 		BeginDrawing();
 		ClearBackground(WHITE);
-		
-		DrawCircle(circle_x, circle_y, circleRadius, BLUE);
-		DrawRectangle(sqaure_x, squre_y, sqaureWidth, sqaureHeight, RED);
 
-		squre_y += squareMoveDir;
-		if (squre_y > (screenHeight-sqaureHeight) || squre_y < 0) squareMoveDir = -squareMoveDir;
+		if (endGameCollision)
+		{
+			DrawText("GAME OVER!!!", screenWidth / 2, screenHeight / 2, 24, RED);
+		}
+		else
+		{
+			//AI squre movement
+			squre_y += squareMoveDir;
 
-		if (IsKeyDown(KEY_W) && circle_y > (0+circleRadius)) circle_y -= movementSpeed;
-		if (IsKeyDown(KEY_S) && circle_y < (screenHeight - circleRadius)) circle_y += movementSpeed;
-		if (IsKeyDown(KEY_A) && circle_x > (0 + circleRadius)) circle_x -= movementSpeed;
-		if (IsKeyDown(KEY_D) && circle_x < (screenWidth-circleRadius)) circle_x += movementSpeed;
+			//cirlce collision bounds
+			int circle_LeftEdge = circle_x - circleRadius;
+			int circle_RightEdge = circle_x + circleRadius;
+			int circle_TopEdge = circle_y - circleRadius;
+			int circle_BottomEdge = circle_y + circleRadius;
+
+			//square collision bounds
+			int squre_LeftEdge = sqaure_x;
+			int squre_RightEdge = sqaure_x + sqaureWidth;
+			int squre_TopEdge = squre_y;
+			int squre_BottomEdge = squre_y + sqaureHeight;
+
+			//draw elements
+			DrawCircle(circle_x, circle_y, circleRadius, BLUE);
+			DrawRectangle(sqaure_x, squre_y, sqaureWidth, sqaureHeight, RED);
+			
+			//check if squre has hit world bounds, redirect if so.
+			if (squre_BottomEdge >= screenHeight || squre_TopEdge <= 0) squareMoveDir = -squareMoveDir;
+
+			//player inputs
+			if (IsKeyDown(KEY_W) && circle_TopEdge > 0) circle_y -= movementSpeed;
+			if (IsKeyDown(KEY_S) && circle_BottomEdge < screenHeight) circle_y += movementSpeed;
+			if (IsKeyDown(KEY_A) && circle_LeftEdge > 0) circle_x -= movementSpeed;
+			if (IsKeyDown(KEY_D) && circle_RightEdge < screenWidth ) circle_x += movementSpeed;
+
+			//Detecting collision between player and squre
+			if (squre_BottomEdge >= circle_TopEdge &&
+				squre_TopEdge <= circle_BottomEdge &&
+				squre_LeftEdge <= circle_RightEdge &&
+				squre_RightEdge >= circle_LeftEdge)
+			{
+				endGameCollision = true;
+			}
+		}
 
 		EndDrawing();
 	}
