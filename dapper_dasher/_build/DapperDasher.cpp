0384f7c6566fee2774605fe13bdfa85b18084a40
Tyler <Tyler.lazarus01@outlook.com>
2023-07-12 23:44:00 +0100

    Dapper Dasher Commit 4: Added Aniamtions to scarfy Started adding in harzards in the form of a nebula enermy Added regions to script to improve readability.

--- dapper_dasher/_build/DapperDasher.cpp
+++ dapper_dasher/_build/DapperDasher.cpp
@@ -2,30 +2,53 @@
 
 int main()
 {
-	//Window Init
+#pragma region Window_Init
+
 	int const WINDOW_WIDTH = 512;
 	int const WINDOW_HEIGHT = 380;
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Dapper Dasher");
 
-	//Global Values
+#pragma endregion
+
+#pragma region Const Values - Magic Numbers
 
 	int const JUMPFORCE = -600;
 	int const GRAVITY = 1000;
 
-	//Player Values
+#pragma endregion
+
+#pragma region Player Variables
+
+	int const PLAYER_ANIMFRAME_MAX = 6;
+	float const PLAYER_ANIMUPDATETIME = 1.0 / 12.0;
+
+	int scarfy_VelocityY = 0;
+	int scarfy_AnimFrame = 0;
+	float scarfy_TimeSinceLastAnimFrame = 0;
+	bool playerIsGrounded = false;
+
 	Texture2D scarfy = LoadTexture("textures/scarfy.png");
+
 	Rectangle scarfyRect;
-	scarfyRect.width = scarfy.width / 6;
+	scarfyRect.width = scarfy.width / PLAYER_ANIMFRAME_MAX;
 	scarfyRect.height = scarfy.height;
 	scarfyRect.x = 0;
 	scarfyRect.y = 0;
+
 	Vector2 scarfyPos;
 	scarfyPos.x = WINDOW_WIDTH / 2 - scarfyRect.width / 2;
 	scarfyPos.y = WINDOW_HEIGHT - scarfyRect.height;
 
-	int scarfy_VelocityY = 0;
+#pragma endregion
 
-	bool playerIsGrounded = false;
+#pragma region Nebula Variables
+
+	int nebula_VelocityX = 0;
+
+	Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
+
+
+#pragma endregion
 
 
 	SetTargetFPS(60);
@@ -61,10 +84,24 @@ int main()
 		//Update player Y POS with Y Velocity
 		scarfyPos.y += scarfy_VelocityY * DELTA_TIME;
 
+		//Animate Scarfy
+		scarfy_TimeSinceLastAnimFrame += DELTA_TIME;
+		if (scarfy_TimeSinceLastAnimFrame >= PLAYER_ANIMUPDATETIME)
+		{
+			scarfy_TimeSinceLastAnimFrame = 0;
+
+			if (scarfy_AnimFrame > PLAYER_ANIMFRAME_MAX)
+				scarfy_AnimFrame == 0;
+			scarfyRect.x = scarfy_AnimFrame * scarfyRect.width;
+			scarfy_AnimFrame++;
+		}
+
+		//Draw Scarfy
 		DrawTextureRec(scarfy, scarfyRect, scarfyPos, WHITE);
 
 		EndDrawing();
 	}
 	UnloadTexture(scarfy);
+	UnloadTexture(nebula);
 	CloseWindow();
 }
